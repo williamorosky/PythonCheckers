40bf33f9dc2a0c63d203fc74d4c6e6eb4c95086d
williamorosky <williamorosky@gmail.com>
2016-11-10 19:40:59 -0600

    Add files via upload

--- checkers.py
+++ checkers.py
@@ -1,61 +1,140 @@
 from Tkinter import *
 
+gameBoardMatrix =  [[ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0]]
+
+gameBoardIdMatrix = [[ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0]]
+
+gamePieceMatrix =  [[ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 0, 0, 0, 0, 0, 0, 0, 0],
+                    [ 0, 0, 0, 0, 0, 0, 0, 0],
+                    [ 2, 0, 2, 0, 2, 0, 2, 0],
+                    [ 0, 2, 0, 2, 0, 2, 0, 2],
+                    [ 2, 0, 2, 0, 2, 0, 2, 0]]
+
+gameIdMatrix =     [[ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 1, 0, 1, 0, 1, 0, 1, 0],
+                    [ 0, 1, 0, 1, 0, 1, 0, 1],
+                    [ 0, 0, 0, 0, 0, 0, 0, 0],
+                    [ 0, 0, 0, 0, 0, 0, 0, 0],
+                    [ 2, 0, 2, 0, 2, 0, 2, 0],
+                    [ 0, 2, 0, 2, 0, 2, 0, 2],
+                    [ 2, 0, 2, 0, 2, 0, 2, 0]]
+isRedTurn = bool(1)
+
 class CheckerBoard(Frame):
 
     def __init__(self, parent):
-        Frame.__init__(self,parent)
-        self.parent = parent
 
+        Frame.__init__(self,parent)
+
+        self.parent = parent
         self.setupGame()
 
     def setupGame(self):
         self.parent.title("Python Checkers")
         canvas = Canvas(self,bg="CadetBlue1")
-
         self.setupBoard(canvas)
         self.setupGamePieces(canvas)
 
     def setupBoard(self, canvas):
         self.pack(fill=BOTH, expand=1)
-        for x in range(0,8,2):
-            for y in range(0,8,2):
-                whiteSquares1 = canvas.create_rectangle(40+(x*50), 30+(y*50), 90+(x*50), 80+(y*50), outline="white", fill="white", tags="white")
-                graySquares1 = canvas.create_rectangle(40+(x*50), 30+((y+1)*50), 90+(x*50), 80+((y+1)*50), outline="white", fill="gray", tags="gray")
+        for (y,row) in enumerate(gameBoardMatrix):
+            for (x,value) in enumerate(row):
+                if value == 1:
+                    graySquare = canvas.create_rectangle(40+(x*50), 30+((y)*50), 90+(x*50), 80+((y)*50), outline="gray", fill="gray", tags="gray")
+                    gameBoardIdMatrix[y][x] = graySquare
+                elif value == 0:
+                    whiteSquare = canvas.create_rectangle(40+(x*50), 30+((y)*50), 90+(x*50), 80+((y)*50), outline="white", fill="white", tags="white")
+                    gameBoardIdMatrix[y][x] = whiteSquare
 
-        for x in range(1,9,2):
-            for y in range(1,9,2):
-                whiteSquares2 = canvas.create_rectangle(40+(x*50), 30+(y*50), 90+(x*50), 80+(y*50), outline="white", fill="white", tags="white")        
-                graySquares2 = canvas.create_rectangle(40+(x*50), 30+((y-1)*50), 90+(x*50), 80+((y-1)*50), outline="white", fill="gray", tags="gray")        
         canvas.pack(fill=BOTH, expand=1)
 
     def setupGamePieces(self, canvas):
         self.pack(fill=BOTH, expand=1)
+        for (y,row) in enumerate(gamePieceMatrix):
+            for (x,value) in enumerate(row):
+                if value == 1:
+                    blackChecker = canvas.create_oval(50+(x*50), 40+((y)*50), 80+(x*50), 70+((y)*50), outline="black", fill="black", tags="black")
+                    gameIdMatrix[y][x] = blackChecker
+                    canvas.tag_bind(blackChecker, "<ButtonPress-1>", lambda event, canvas=canvas:self.clickPiece(event, canvas)) 
+                elif value == 2:
+                    redChecker = canvas.create_oval(50+(x*50), 40+((y)*50), 80+(x*50), 70+((y)*50), outline="red", fill="red", tags="red")
+                    gameIdMatrix[y][x] = redChecker
+                    canvas.tag_bind(redChecker, "<ButtonPress-1>", lambda event, canvas=canvas:self.clickPiece(event, canvas)) 
 
-        #BLACK PIECES
-        for x in range(0,8,2):
-            for y in range(0,8,2):
-                if y<2:
-                    re1 = canvas.create_oval(50+(x*50), 40+((y+1)*50), 80+(x*50), 70+((y+1)*50), outline="black", fill="black", tags="black")
+    def clickPiece(self, event, canvas):
+        x = canvas.canvasx(event.x)
+        y = canvas.canvasy(event.y)
+        idValue = canvas.find_closest(x, y)[0]
+        for (y,row) in enumerate(gameIdMatrix):
+            for (x,value) in enumerate(row):
+                if value == idValue:
+                    self.showValidMoves(y, x, canvas)
 
-        for x in range(1,9,2):
-            for y in range(1,9,2):
-                if y<4:
-                    re2 = canvas.create_oval(50+(x*50), 40+((y-1)*50), 80+(x*50), 70+((y-1)*50), outline="black", fill="black", tags="black")        
-        
-        #RED PIECES
-        for x in range(0,8,2):
-            for y in range(0,8,2):
-                if y>3:
-                    re1 = canvas.create_oval(50+(x*50), 40+((y+1)*50), 80+(x*50), 70+((y+1)*50), outline="red", fill="red", tags="red")
+    def showValidMoves(self, row, column, canvas):
 
-        for x in range(1,9,2):
-            for y in range(1,9,2):
-                if y>5:
-                    re2 = canvas.create_oval(50+(x*50), 40+((y-1)*50), 80+(x*50), 70+((y-1)*50), outline="red", fill="red", tags="red")        
+        if not isRedTurn and gamePieceMatrix[row][column] == 1 :
+            if gamePieceMatrix[row+1][column-1] == 0:
+                print("Valid Left")                
+                canvas.itemconfig((((row+1)*8)+column), outline="blue")
+                canvas.tag_bind((((row+1)*8)+column), "<ButtonPress-1>", lambda event, row=row, column=column, canvas=canvas:self.executeMove(event, row, column, canvas))
+            if gamePieceMatrix[row+1][column+1] == 0:
+                print("Valid Right")
+                canvas.itemconfig((((row+1)*8)+column+2), outline="blue")
+                canvas.tag_bind((((row+1)*8)+column+2), "<ButtonPress-1>", lambda event, row=row, column=column, canvas=canvas:self.executeMove(event, row, column, canvas))
+        elif isRedTurn and gamePieceMatrix[row][column] == 2 :
+            if gamePieceMatrix[row-1][column-1] == 0:
+                print("Valid Right")
+                canvas.itemconfig((((row-1)*8)+column), outline="blue")
+                canvas.tag_bind((((row-1)*8)+column), "<ButtonPress-1>", lambda event, row=row, column=column, canvas=canvas:self.executeMove(event, row, column, canvas))
+            if gamePieceMatrix[row-1][column+1] == 0:
+                print("Valid Left")
+                canvas.itemconfig((((row-1)*8)+column+2), outline="blue")
+                canvas.tag_bind((((row-1)*8)+column+2), "<ButtonPress-1>", lambda event, row=row, column=column, canvas=canvas:self.executeMove(event, row, column, canvas))
 
+    def executeMove(self, event, row, column, canvas):
+        print "Move"
+        x = canvas.canvasx(event.x)
+        y = canvas.canvasy(event.y)
+        newRow = 0
+        newColumn = 0
+        oldRow = row
+        oldColumn = column
+        newPosition = canvas.find_closest(x, y)[0]
+        for (y,row) in enumerate(gameBoardMatrix):
+            for (x,value) in enumerate(row):
+                if value == newPosition:
+                    newRow = y
+                    newColumn = x
 
-        canvas.pack(fill=BOTH, expand=1)
+        gamePieceMatrix[newRow][newColumn] = gamePieceMatrix[oldRow][oldColumn]
+        gamePieceMatrix[oldRow][oldColumn] = 0
+        gameIdMatrix[newRow][newColumn] = gameIdMatrix[oldRow][oldColumn]
+        gameIdMatrix[oldRow][oldColumn] = 0
 
+        canvas.itemconfig((((oldRow+1)*8)+oldColumn+2), outline="gray")
+        canvas.itemconfig((((oldRow+1)*8)+oldColumn), outline="gray")
+        canvas.itemconfig((((oldRow-1)*8)+oldColumn+2), outline="gray")
+        canvas.itemconfig((((oldRow-1)*8)+oldColumn), outline="gray")
+        global isRedTurn
+        isRedTurn = not isRedTurn
+                        
 
 def main():
     root = Tk()
@@ -64,6 +143,7 @@ def main():
     root.resizable(width=False, height=False)
     checkerboard = CheckerBoard(root)
     root.mainloop()
+    
 
 if __name__ == '__main__':
     main()
